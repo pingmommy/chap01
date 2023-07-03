bb38915a570330f696ca032d02aab426b2efe6f1
mcar8764 <mcar8764@naver.com>
2023-07-03 12:11:29 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+class Helloworld
+
+
+
+{
+
+	public static void main(String[] args){
+
+		System.out.println("Hello");  }
+		System.out.println("World");  }
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Triangle.java
@@ -0,0 +1,28 @@
+
+
+
+class Triangle{
+
+
+
+	public static void main(String[] args){
+
+			System.out.println("*"); 
+			System.out.println("**"); 
+			System.out.println("***"); 
+			System.out.println("****"); 
+			System.out.println("******");
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
