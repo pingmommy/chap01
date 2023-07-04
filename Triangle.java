ba43a5ff8dc0d85ee6421dc4db7b167797acbc8e
mcar8764 <mcar8764@naver.com>
2023-07-04 18:09:11 +0900

    space

--- Triangle.java
+++ Triangle.java
@@ -8,7 +8,7 @@ class Triangle{
 	public static void main(String[] args){
 
 			System.out.println("######");
-			System.out.println("####"); 
+			System.out.println("  ####"); 
 			System.out.println("###"); 
 			System.out.println("##"); 
 			System.out.println("#"); 
