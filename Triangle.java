f9d2fb64b4b1df9b19993b1ef5ddf3297c106a3b
mcar8764 <mcar8764@gmail.com>
2023-07-04 17:15:45 +0900

    수수ㅑ슈

--- Triangle.java
+++ Triangle.java
@@ -7,11 +7,11 @@ class Triangle{
 
 	public static void main(String[] args){
 
-			System.out.println("#"); 
-			System.out.println("##"); 
-			System.out.println("###"); 
-			System.out.println("####"); 
 			System.out.println("######");
+			System.out.println("####"); 
+			System.out.println("###"); 
+			System.out.println("##"); 
+			System.out.println("#"); 
 
 
 
