946b60f876f4a7717064ec55a44fe81151099963
mcar8764 <mcar8764@naver.com>
2023-07-06 12:10:09 +0900

    color fg bg

--- src/Hello.java
+++ src/Hello.java
@@ -10,9 +10,11 @@ public class Hello
 
 	public static void main(String[] args){
 		
-		System.out.println("033[2j");  
+		System.out.println("\033[2j");  
+		System.out.println("\033[31m");  
+		System.out.println("\033[44m");  
 		System.out.println("Hello");  
-		System.out.println("World");  }
+		System.out.println("\033[0m");  }
 
  }
 
