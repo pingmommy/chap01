0fe861b432f8bcc7532ee2e1e00112f95091e36a
mcar8764 <mcar8764@gmail.com>
2023-07-06 13:33:23 +0900

    Modified javafile

--- src/Hello.java
+++ src/Hello.java
@@ -9,12 +9,31 @@ public class Hello
 {
 
 	public static void main(String[] args){
-		
-		System.out.println("\033[2j");  
+		 System.out.println("\033[2J");
 		System.out.println("\033[31m");  
 		System.out.println("\033[44m");  
-		System.out.println("Hello");  
-		System.out.println("\033[0m");  }
+		System.out.print("\033[10;20H");	
+		System.out.println("Hello Java");  
 
+		
+		System.out.println("\033[34m");  
+		System.out.println("\033[41m");  
+		System.out.print("\033[5;10H");	
+		System.out.println("Hello JavaScript");  
+
+
+		System.out.println("\033[31m");  
+		System.out.println("\033[44m");  
+		System.out.print("\033[20;3H");	
+		System.out.println("Hello HTML");  
+
+
+		System.out.println("\033[34m");  
+		System.out.println("\033[41m");  
+		System.out.print("\033[15;30H");	
+		System.out.println("Hello SQL");  
+                
+		System.out.println("\033[0m");
+	}
  }
 
